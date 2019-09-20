f1eea581228e243c6c452fe0b6f943022b95fce6
alberto <alan.ac07@gmail.com>
2019-09-20 09:19:28 -0500

    lol

--- classification.py
+++ classification.py
@@ -1,20 +1,40 @@
 import numpy as np
-from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
 # load data using sklearn dataset
-data = load_breast_cancer()
+data = pd.read_csv("APFNBTraining.csv")
+y = data['90'] # target
+X = data.drop(['90', 'Producto', 'Contrato'], axis = 1) #
 
 # Split the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.33)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+print("\ntrain:\n")
+print(X_train.shape)
+print(y_train.shape)
+
+print("\ntest:\n")
+print(X_test.shape)
+print(y_test.shape)
 
 # implement RandomForest to train our test set
-model = RandomForestClassifier()
+model = GaussianNB()
 model.fit(X_train, y_train)
 
-predictions = model.predict(X_test)
+print("score:", model.score(X_test, y_test))
+
+# load data to predict
+
+pred = pd.read_csv("APFNBPredict.csv")
+
+predfnb = pred.drop(['Producto', 'Contrato'], axis=1)
+print("\nFNB:\n")
+print(predfnb.shape)
+
+predictions = model.predict(predfnb)
 
 
-
+predfnb['target'] = predictions
+predfnb.to_csv('predictions.csv')
 
